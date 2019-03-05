d1b9b5405bf14e503c97760640b78c40a2df3c24
abbkevkla <43879859+abbkevkla@users.noreply.github.com>
2019-03-05 14:53:51 +0100

    Create Kompendium_uppgifter_3.2.py

---
+++ Kompendium_uppgifter_3.2.py
@@ -0,0 +1,44 @@
+male = [
+    "Erik",
+    "Lars",
+    "Karl",
+    "Anders",
+    "Johan"
+]
+
+female = [
+    "Maria",
+    "Anna",
+    "Margareta",
+    "Elisabeth",
+    "Eva"
+]
+
+#3.1
+# print(male[0])
+# print(female[2])
+# print(female[4])
+# print(male[1])
+
+#3.2
+# del(male[2])
+# del(female[0])
+# del(male[1])
+
+#3.3
+#male.append("Kevin")
+
+#3.4
+# male.sort()
+# female.sort() #sorterar listorna i alfabetsordning
+
+#3.5
+# namn=input("välj namn att ta bort: ")
+# if namn in male:
+#     male.remove(namn)
+# if namn in female:
+#     female.remove(namn)
+
+######################
+print('Män: ', male)
+print('Kvinnor: ', female)
