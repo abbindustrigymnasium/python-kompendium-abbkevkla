853a5cb47291ca6d823b96cabc405c45c9fde14d
abbkevkla <43879859+abbkevkla@users.noreply.github.com>
2019-03-05 15:17:29 +0100

    Create Kompendium_uppgifter_4.4.py

---
+++ Kompendium_uppgifter_4.4.py
@@ -0,0 +1,31 @@
+#4.1
+# magiska_talet=0
+# for i in range(0, 100000):
+#     magiska_talet=magiska_talet+i
+# print(magiska_talet)
+
+#4.2
+# magiska_tal2=0
+# for i in range(0, 500):
+#     if i%2 != 0:
+#         magiska_tal2=magiska_tal2+i
+# print(magiska_tal2)
+
+#4.3
+# registrerade=["Anna", "Eva", "Erik", "Lars", "Karl"]
+# avanmälningar=["Anna", "Erik", "Karl"]
+# for namn in registrerade:
+#     if namn in avanmälningar:
+#         registrerade.remove(namn)
+# print(registrerade)
+
+#4.4
+# förnamn=["Maria", "Erik", "Karl"]
+# efternamn=["Svensson", "Karlsson", "Andersson"]
+# helnamn=[]
+# for FN in förnamn:
+#     for EN in efternamn:
+#         helnamn.append(FN+" "+EN)
+# print(helnamn)
+
+ 
